cbffb588db5d8fcbe179719d934c9867e5ec27b2
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-02-11 10:14:49 -0500

    Challenge: Change greeting in index-with-require by time

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -7,8 +7,19 @@ const today = require('./today');
 // Define the request listener function
 const requestListener = function (req, res) {
     res.writeHead(200); // Set the status code to 200 (OK)
+    let dateVal = today.getDate();
     // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
+    let greeting = "It is still not morning.";
+    if(dateVal.getHours() > 6 && dateVal.getHours() < 12) {
+        greeting = "Good Morning!"
+    } else if(dateVal.getHours() > 12 && dateVal.getHours() < 18){
+        greeting = "Good Afternoon!"
+    } else if(dateVal.getHours() > 18 && dateVal.getHours() < 21) {
+        greeting = "Good Evening!"
+    } else if(dateVal.getHours() > 21 && dateVal.getHours() < 24) {
+        greeting = "Good Night!"
+    }
+    res.end(`${greeting} \n Hello, World! The date today is ${today.getDate()}`);
 };
 
 // Define the port number
